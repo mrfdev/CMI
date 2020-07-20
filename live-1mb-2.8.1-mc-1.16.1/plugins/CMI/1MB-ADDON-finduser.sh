5da9eb37f9876f590eb5be7af7223b7ed8936d56
mrfloris <mrfloris@gmail.com>
2020-07-20 20:54:51 +0200

    - updated script to version 0.0.2 - now takes argument from cli - tries to detect uuid vs username - prints out multiple matches - now exits on empty argument - now exits on too many arguments

--- live-1mb-2.8.1-mc-1.16.1/plugins/CMI/1MB-ADDON-finduser.sh
+++ live-1mb-2.8.1-mc-1.16.1/plugins/CMI/1MB-ADDON-finduser.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
-# Quick tool to get uuid/username from CMI database.
+# Quick 1MB CMI Add-on to get uuid/username from cmi.sqlite.db
 # chmod a+x the .sh script, and run it with ./finduser.sh
-# version 0.0.1
-dbresult=$(sqlite3 cmi.sqlite.db "SELECT player_uuid,userName FROM \"main\".\"users\" WHERE \"userName\" LIKE '%mrfloris%' ESCAPE '\' ORDER BY \"_rowid_\" ASC LIMIT 0, 49999")
-echo -e $dbresult
+# version 0.0.2
+
+findUser="$1"
+
+if [ "$findUser" == "" ]; then
+	echo -e "Syntax: ./${0} <user|uuid> (error, missing argument)"; exit 0
+elif [ $# -gt 1 ]; then
+    echo -e "Syntax: ./${0} <user|uuid> (error, you had too many arguments)"; exit 0
+fi
+
+[[ ${#findUser} -gt 25 ]] && findType="player_uuid" || findType="userName"
+
+dbresult=$(sqlite3 cmi.sqlite.db "SELECT player_uuid,userName FROM \"main\".\"users\" WHERE $findType LIKE '%$findUser%' ESCAPE '\' ORDER BY \"_rowid_\" ASC LIMIT 0, 49999")
+echo -e "$dbresult"
