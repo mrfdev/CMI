d0da45eecaed89ba14569da03127c4e668453af2
mrfloris <mrfloris@gmail.com>
2020-07-25 00:45:28 +0200

    bump version finduser script

--- Resources/Scripts/finduser.sh
+++ Resources/Scripts/finduser.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 # Quick 1MB CMI Add-on to get uuid/username from cmi.sqlite.db
 # chmod a+x the .sh script, and run it with ./finduser.sh
-# version 0.0.2
+# version: 1.0.0 build 0.0.3
+# Release: July 25th, 2020
+# Author: Floris Fiedeldij Dop
 
 findUser="$1"
 
