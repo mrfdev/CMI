b6bddad736cd03753aa6d757eb684487c70211de
mrfloris <mrfloris@gmail.com>
2022-03-25 20:16:37 +0100

    - First 1.1.x update * update to Semantic versioning * bump to build 10 * tested with CMI 9.1.3.2 * replaced a few tabs with spaces * replaced raw query result output with for loop * now includes namemc url link to found uuid * now includes easy to copy paste cmi info/checkaccount commands * not tested on ubuntu, only tested on macOS Catalina * from previous commit: updated header, version, config/donotconfig and footer

--- Resources/Scripts/1MB-finduser.sh
+++ Resources/Scripts/1MB-finduser.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # @Filename: 1MB-finduser.sh
-# @Version: 1.0.3, build 009
+# @Version: 1.1.0, build 010
 # @Release: March 25th, 2022
 # @Description: Quick 1MB Add-on to get CMI uuid/username from cmi.sqlite.db
 # @Contact: I am @floris on Twitter, and mrfloris in MineCraft.
@@ -20,6 +20,9 @@
 # requires cli input, no need to really edit anything
 findUser="$1"
 
+# url to query uuid with
+findUrl="https://namemc.com/search?q="
+
 ### FUNCTIONS AND CODE
 #
 # ! WE ARE DONE, STOP EDITING BEYOND THIS POINT !
@@ -27,7 +30,7 @@ findUser="$1"
 ###
 
 if [ "$findUser" == "" ]; then
-	echo -e "Syntax: ${0} <user|uuid> (error, missing argument)"; exit 0
+    echo -e "Syntax: ${0} <user|uuid> (error, missing argument)"; exit 0
 elif [ $# -gt 1 ]; then
     echo -e "Syntax: ${0} <user|uuid> (error, you had too many arguments)"; exit 0
 fi
@@ -35,6 +38,16 @@ fi
 [[ ${#findUser} -gt 25 ]] && findType="player_uuid" || findType="userName"
 
 dbresult=$(sqlite3 cmi.sqlite.db "SELECT player_uuid,userName FROM \"main\".\"users\" WHERE $findType LIKE '%$findUser%' ESCAPE '\' ORDER BY \"_rowid_\" ASC LIMIT 0, 49999")
-echo -e "$dbresult"
+for i in $dbresult; do
+    oIFS=$IFS
+    IFS='|'
+    y=($i)
+    IFS=$oIFS
+    uuidResult="${y[0]}"
+    userResult="${y[1]}"
+    echo -e "uuid: $uuidResult | $findUrl${uuidResult}"
+    echo -e "user: $userResult | cmi info $uuidResult | cmi checkaccount $uuidResult \n"
+done
+
 
 #EOF Copyright (c) 2011-2022 - Floris Fiedeldij Dop - https://scripts.1moreblock.com
