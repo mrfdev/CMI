452a055ad6a0f65acced75e9b6568eaf7beb9baf
Floris <mrfloris@gmail.com>
2023-01-23 07:57:17 +0100

    Update 1MB-zripsplugins.sh
    - Added footer copyright
    - added header, so it's more consistent with the other .sh scripts I make
    - bumped version to 0.0.4, build 006
    - split config settings vs internal config settings
    - split config vs the code they shouldn't touch
    - included my theme, so things are little more colorful
    - option to toggle on debug mode for the extra info
    - included _output function so the output is more consistent 
    - slightly updated how things visually display
    - improved the #comments above the code to explain what it does a bit better
    - further improved the resource queries to be more dynamic, prepping for a future function
    - set debug to false by default.
    - this is now usable code, you should be able to use this on most ubuntu releases, macos, maybe even windows git-bash, requires jq, but have only tested it on one shell. consider this "an early alpha" I guess.

--- Resources/Scripts/1MB-zripsplugins.sh
+++ Resources/Scripts/1MB-zripsplugins.sh
@@ -1,96 +1,360 @@
 #!/usr/bin/env bash
 
-# zrips plugins downloader / list, version 0.0.3, build 004, by floris
+# @Filename: 1MB-zripsplugins.sh
+# @Version: 0.0.4, build 006
+# @Release: January 23rd, 2023
+# @Description: Helps us get a list of links to latest versions of the Zrips plugins
+# @Contact: I am @floris on Twitter, and mrfloris in MineCraft.
+# @Discord: floris#0233 on https://discord.gg/floris
+# @Install: chmod a+x 1MB-zripsplugins.sh
+# @Syntax: .1MB-zripsplugins.sh
+# @URL: Latest source, info, & support: https://scripts.1moreblock.com/
 
-#config
+### CONFIGURATION
+#
+# Declarations here you can customize to your preferred setup.
+# Generally only if you actually have to. Check Wiki for details.
+#
+###
 
+# The URL to Spigot's website
 _spigotURL="https://www.spigotmc.org"
+
+# The URL to Spiget's API website
 _spigetURL="https://api.spiget.org/v2/resources/"
-_sleepTime="2"
+
+# The delay (in seconds) between each plugin-check
+# Let's be nice and not flood these sites
+_sleepTime="3"
+
+# Custom user-agent string
 _userAgent="VersionCheck/1.0"
 
+### INTERNAL CONFIGURATION
+#
+# Configuration variables you should probably
+# leave alone, but can change if really needed.
+#
+###
+
+# Not in use (yet), but one is for getting the json data, the other the .jar
 _jsonGetter="curl -f -L -s" #todo
-_jarDownload="curl -f -L -s -o" #todo
+_jarDownload="curl -f -L -s -o"
 # 503 dl $_jarDownload "$_apiFile" "$_spigotURL/$_apiDLurl"
 
-# lets go (no need to config past this point)
+_debug=false # Set to false to minimize output.
 
-echo -e "\\nfree to download"
+Y="\\033[33m"; C="\\033[36m"; R="\\033[0m" # theme
 
-#cmilib
+### END OF CONFIGURATION
+#
+# Really stop configuring things
+# beyond this point. I mean it.
+#
+###
+
+function _output {
+    case "$1" in
+    oops)
+        _args="${*:2}"; _prefix="(Script Halted!)";
+        echo -e "\\n$B$Y$_prefix$X $_args $R" >&2; exit 1
+    ;;
+    okay)
+        _args="${*:2}"; _prefix="(Info)";
+        echo -e "\\n$B$Y$_prefix$C $_args $R" >&2; exit 1
+    ;;
+    debug)
+        _args="${*:2}"; _prefix="(Debug)";
+        [[ "$_debug" == true ]] && echo -e "$Y$_prefix$C $_args $R"
+    ;;
+    *)
+        _args="${*:1}"; _prefix="(Info)";
+        echo -e "$Y$_prefix$C $_args $R"
+    ;;
+    esac
+}
+
+[ "$EUID" -eq 0 ] && _output oops "*!* This script should not be run using sudo, or as the root user!"
+
+
+# List the Zrips Free plugin releases
+_output "List of links to latest versions of the Zrips plugins"
+_output "> Free releases:"
+
+##############
+# CMILib
+##############
+# resource id
 _apiResource="87610"
-#name
+
+# resource name
 _apiDLname=$(curl -f -L -s --user-agent "$_userAgent" $_spigetURL/$_apiResource | jq -r ".name")
-echo -e "name: $_apiDLname"
+[[ "$_debug" == true ]] && echo -e " "
+_output debug "name: $_apiDLname"
 
-#latestid
+# resource latest release id
 _apiDLid=$(curl -f -L -s --user-agent "$_userAgent" $_spigetURL/$_apiResource/versions/latest | jq -r ".id")
-echo -e "id: $_apiDLid"
+_output debug "id: $_apiDLid"
 
-#versionnumber
+# resource latest version number
 _apiDLversion=$(curl -f -L -s --user-agent "$_userAgent" $_spigetURL/$_apiResource/versions/$_apiDLid | jq -r ".name")
-echo -e "ver: $_apiDLversion"
+_output debug "ver: $_apiDLversion"
 
-#filename
+# resource filename
 _apiFile="$_apiDLname-$_apiDLversion.jar"
-echo -e "file: $_apiFile"
+_output debug "file: $_apiFile"
 
-#downloadurl
+# resource download url
 _apiDLurl=$(curl -f -L -s --user-agent "$_userAgent" $_spigetURL/$_apiResource | jq -r ".file.url")
-echo -e "ddl: $_spigotURL/$_apiDLurl"
+_output "$_spigotURL/$_apiDLurl"
 
-exit 1
+# lets take a little break
 sleep $_sleepTime
 
+##############
+# Jobs-ReBorn
+##############
+# resource id
+_apiResource="4216"
 
+# resource name
+_apiDLname=$(curl -f -L -s --user-agent "$_userAgent" $_spigetURL/$_apiResource | jq -r ".name")
+[[ "$_debug" == true ]] && echo -e " "
+_output debug "name: $_apiDLname"
 
+# resource latest release id
+_apiDLid=$(curl -f -L -s --user-agent "$_userAgent" $_spigetURL/$_apiResource/versions/latest | jq -r ".id")
+_output debug "id: $_apiDLid"
 
-#jobs
-_apiDLurl=$(curl -f -L -s --user-agent "$_userAgent" $_spigetURL/4216 | jq -r ".file.url")
-echo -e "$_spigotURL/$_apiDLurl"
+# resource latest version number
+_apiDLversion=$(curl -f -L -s --user-agent "$_userAgent" $_spigetURL/$_apiResource/versions/$_apiDLid | jq -r ".name")
+_output debug "ver: $_apiDLversion"
 
+# resource filename
+_apiFile="$_apiDLname-$_apiDLversion.jar"
+_output debug "file: $_apiFile"
+
+# resource download url
+_apiDLurl=$(curl -f -L -s --user-agent "$_userAgent" $_spigetURL/$_apiResource | jq -r ".file.url")
+_output "$_spigotURL/$_apiDLurl"
+
+# lets take a little break
 sleep $_sleepTime
 
-#jobs
-_apiDLurl=$(curl -f -L -s --user-agent "$_userAgent" $_spigetURL/2815 | jq -r ".file.url")
-echo -e "$_spigotURL/$_apiDLurl"
+##############
+# BottledExp
+##############
+# resource id
+_apiResource="2815"
 
+# resource name
+_apiDLname=$(curl -f -L -s --user-agent "$_userAgent" $_spigetURL/$_apiResource | jq -r ".name")
+[[ "$_debug" == true ]] && echo -e " "
+_output debug "name: $_apiDLname"
+
+# resource latest release id
+_apiDLid=$(curl -f -L -s --user-agent "$_userAgent" $_spigetURL/$_apiResource/versions/latest | jq -r ".id")
+_output debug "id: $_apiDLid"
+
+# resource latest version number
+_apiDLversion=$(curl -f -L -s --user-agent "$_userAgent" $_spigetURL/$_apiResource/versions/$_apiDLid | jq -r ".name")
+_output debug "ver: $_apiDLversion"
+
+# resource filename
+_apiFile="$_apiDLname-$_apiDLversion.jar"
+_output debug "file: $_apiFile"
+
+# resource download url
+_apiDLurl=$(curl -f -L -s --user-agent "$_userAgent" $_spigetURL/$_apiResource | jq -r ".file.url")
+_output "$_spigotURL/$_apiDLurl"
+
+# lets take a little break
 sleep $_sleepTime
 
-echo -e "\\npremium releases"
+# List the Zrips Premium plugin releases
+_output "> Premium releases:"
 
-#trademe
-_apiDLurl=$(curl -f -L -s --user-agent "$_userAgent" $_spigetURL/7544 | jq -r ".file.url")
-echo -e "$_spigotURL/$_apiDLurl"
+##############
+# TradeMe
+##############
+# resource id
+_apiResource="7544"
 
+# resource name
+_apiDLname=$(curl -f -L -s --user-agent "$_userAgent" $_spigetURL/$_apiResource | jq -r ".name")
+[[ "$_debug" == true ]] && echo -e " "
+_output debug "name: $_apiDLname"
+
+# resource latest release id
+_apiDLid=$(curl -f -L -s --user-agent "$_userAgent" $_spigetURL/$_apiResource/versions/latest | jq -r ".id")
+_output debug "id: $_apiDLid"
+
+# resource latest version number
+_apiDLversion=$(curl -f -L -s --user-agent "$_userAgent" $_spigetURL/$_apiResource/versions/$_apiDLid | jq -r ".name")
+_output debug "ver: $_apiDLversion"
+
+# resource filename
+_apiFile="$_apiDLname-$_apiDLversion.jar"
+_output debug "file: $_apiFile"
+
+# resource download url
+_apiDLurl=$(curl -f -L -s --user-agent "$_userAgent" $_spigetURL/$_apiResource | jq -r ".file.url")
+_output "$_spigotURL/$_apiDLurl"
+
+# lets take a little break
 sleep $_sleepTime
 
-#residence
-_apiDLurl=$(curl -f -L -s --user-agent "$_userAgent" $_spigetURL/11480 | jq -r ".file.url")
-echo -e "$_spigotURL/$_apiDLurl"
+##############
+# Residence
+##############
+# resource id
+_apiResource="11480"
 
+# resource name
+_apiDLname=$(curl -f -L -s --user-agent "$_userAgent" $_spigetURL/$_apiResource | jq -r ".name")
+[[ "$_debug" == true ]] && echo -e " "
+_output debug "name: $_apiDLname"
+
+# resource latest release id
+_apiDLid=$(curl -f -L -s --user-agent "$_userAgent" $_spigetURL/$_apiResource/versions/latest | jq -r ".id")
+_output debug "id: $_apiDLid"
+
+# resource latest version number
+_apiDLversion=$(curl -f -L -s --user-agent "$_userAgent" $_spigetURL/$_apiResource/versions/$_apiDLid | jq -r ".name")
+_output debug "ver: $_apiDLversion"
+
+# resource filename
+_apiFile="$_apiDLname-$_apiDLversion.jar"
+_output debug "file: $_apiFile"
+
+# resource download url
+_apiDLurl=$(curl -f -L -s --user-agent "$_userAgent" $_spigetURL/$_apiResource | jq -r ".file.url")
+_output "$_spigotURL/$_apiDLurl"
+
+# lets take a little break
 sleep $_sleepTime
 
-#tryme
-_apiDLurl=$(curl -f -L -s --user-agent "$_userAgent" $_spigetURL/3330 | jq -r ".file.url")
-echo -e "$_spigotURL/$_apiDLurl"
+##############
+# TryMe
+##############
+# resource id
+_apiResource="3330"
 
+# resource name
+_apiDLname=$(curl -f -L -s --user-agent "$_userAgent" $_spigetURL/$_apiResource | jq -r ".name")
+[[ "$_debug" == true ]] && echo -e " "
+_output debug "name: $_apiDLname"
+
+# resource latest release id
+_apiDLid=$(curl -f -L -s --user-agent "$_userAgent" $_spigetURL/$_apiResource/versions/latest | jq -r ".id")
+_output debug "id: $_apiDLid"
+
+# resource latest version number
+_apiDLversion=$(curl -f -L -s --user-agent "$_userAgent" $_spigetURL/$_apiResource/versions/$_apiDLid | jq -r ".name")
+_output debug "ver: $_apiDLversion"
+
+# resource filename
+_apiFile="$_apiDLname-$_apiDLversion.jar"
+_output debug "file: $_apiFile"
+
+# resource download url
+_apiDLurl=$(curl -f -L -s --user-agent "$_userAgent" $_spigetURL/$_apiResource | jq -r ".file.url")
+_output "$_spigotURL/$_apiDLurl"
+
+# lets take a little break
 sleep $_sleepTime
 
-#recount
-_apiDLurl=$(curl -f -L -s --user-agent "$_userAgent" $_spigetURL/3962 | jq -r ".file.url")
-echo -e "$_spigotURL/$_apiDLurl"
+##############
+# Recount
+##############
+# resource id
+_apiResource="3962"
 
+# resource name
+_apiDLname=$(curl -f -L -s --user-agent "$_userAgent" $_spigetURL/$_apiResource | jq -r ".name")
+[[ "$_debug" == true ]] && echo -e " "
+_output debug "name: $_apiDLname"
+
+# resource latest release id
+_apiDLid=$(curl -f -L -s --user-agent "$_userAgent" $_spigetURL/$_apiResource/versions/latest | jq -r ".id")
+_output debug "id: $_apiDLid"
+
+# resource latest version number
+_apiDLversion=$(curl -f -L -s --user-agent "$_userAgent" $_spigetURL/$_apiResource/versions/$_apiDLid | jq -r ".name")
+_output debug "ver: $_apiDLversion"
+
+# resource filename
+_apiFile="$_apiDLname-$_apiDLversion.jar"
+_output debug "file: $_apiFile"
+
+# resource download url
+_apiDLurl=$(curl -f -L -s --user-agent "$_userAgent" $_spigetURL/$_apiResource | jq -r ".file.url")
+_output "$_spigotURL/$_apiDLurl"
+
+# lets take a little break
 sleep $_sleepTime
 
-#mobfarmmanager
-_apiDLurl=$(curl -f -L -s --user-agent "$_userAgent" $_spigetURL/15127 | jq -r ".file.url")
-echo -e "$_spigotURL/$_apiDLurl"
+##############
+# MobFarmManager
+##############
+# resource id
+_apiResource="15127"
 
+# resource name
+_apiDLname=$(curl -f -L -s --user-agent "$_userAgent" $_spigetURL/$_apiResource | jq -r ".name")
+[[ "$_debug" == true ]] && echo -e " "
+_output debug "name: $_apiDLname"
+
+# resource latest release id
+_apiDLid=$(curl -f -L -s --user-agent "$_userAgent" $_spigetURL/$_apiResource/versions/latest | jq -r ".id")
+_output debug "id: $_apiDLid"
+
+# resource latest version number
+_apiDLversion=$(curl -f -L -s --user-agent "$_userAgent" $_spigetURL/$_apiResource/versions/$_apiDLid | jq -r ".name")
+_output debug "ver: $_apiDLversion"
+
+# resource filename
+_apiFile="$_apiDLname-$_apiDLversion.jar"
+_output debug "file: $_apiFile"
+
+# resource download url
+_apiDLurl=$(curl -f -L -s --user-agent "$_userAgent" $_spigetURL/$_apiResource | jq -r ".file.url")
+_output "$_spigotURL/$_apiDLurl"
+
+# lets take a little break
 sleep $_sleepTime
 
-#SelectionVisualizer
-_apiDLurl=$(curl -f -L -s --user-agent "$_userAgent" $_spigetURL/22631 | jq -r ".file.url")
-echo -e "$_spigotURL/$_apiDLurl"
+##############
+# SelectionVisualizer
+##############
+# resource id
+_apiResource="22631"
 
-echo -e "\\nDone."
+# resource name
+_apiDLname=$(curl -f -L -s --user-agent "$_userAgent" $_spigetURL/$_apiResource | jq -r ".name")
+[[ "$_debug" == true ]] && echo -e " "
+_output debug "name: $_apiDLname"
+
+# resource latest release id
+_apiDLid=$(curl -f -L -s --user-agent "$_userAgent" $_spigetURL/$_apiResource/versions/latest | jq -r ".id")
+_output debug "id: $_apiDLid"
+
+# resource latest version number
+_apiDLversion=$(curl -f -L -s --user-agent "$_userAgent" $_spigetURL/$_apiResource/versions/$_apiDLid | jq -r ".name")
+_output debug "ver: $_apiDLversion"
+
+# resource filename
+_apiFile="$_apiDLname-$_apiDLversion.jar"
+_output debug "file: $_apiFile"
+
+# resource download url
+_apiDLurl=$(curl -f -L -s --user-agent "$_userAgent" $_spigetURL/$_apiResource | jq -r ".file.url")
+_output "$_spigotURL/$_apiDLurl"
+
+# lets take a little break
+sleep $_sleepTime
+
+# Report that we have finished listing the plugins.
+_output okay "\\nDone."
+
+#EOF Copyright (c) 2011-2023 - Floris Fiedeldij Dop - https://scripts.1moreblock.com
