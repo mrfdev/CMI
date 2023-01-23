395e2045403635dbccd4c951c38951732b880896
Floris <mrfloris@gmail.com>
2023-01-23 07:04:02 +0100

    Update 1MB-zripsplugins.sh
    changelog for 0.0.2, build 003:
    - added all the resources, and split them up between free and premium for convenience
    - added repeating strings and URLs into their unique variables
    - added a little bit of a sleep delay between queries, so we don't flood sites
    - tried to add a unique user-agent, no idea if that worked (untested)
    - split up free to download, and premium, so we know which is which 
    - bumped version to 0.0.2, this is build 003.

--- Resources/Scripts/1MB-zripsplugins.sh
+++ Resources/Scripts/1MB-zripsplugins.sh
@@ -1,10 +1,68 @@
+```bash
 #!/usr/bin/env bash
 
-# zrips plugins downloader / list, version 0.0.1, build 002, by floris
+# zrips plugins downloader / list, version 0.0.2, build 003, by floris
 
 _spigotURL="https://www.spigotmc.org"
 _spigetURL="https://api.spiget.org/v2/resources/"
+_sleepTime="2"
+_userAgent="VersionCheck/1.0"
+# --user-agent "$_userAgent"
+echo -e "\\nfree to download"
 
 #cmilib
-_apiDLurl=$(curl -f -L -s $_spigetURL/87610 | jq -r ".file.url")
+_apiDLurl=$(curl -f -L -s --user-agent "$_userAgent" $_spigetURL/87610 | jq -r ".file.url")
 echo -e "$_spigotURL/$_apiDLurl"
+
+sleep $_sleepTime
+
+#jobs
+_apiDLurl=$(curl -f -L -s --user-agent "$_userAgent" $_spigetURL/4216 | jq -r ".file.url")
+echo -e "$_spigotURL/$_apiDLurl"
+
+sleep $_sleepTime
+
+#jobs
+_apiDLurl=$(curl -f -L -s --user-agent "$_userAgent" $_spigetURL/2815 | jq -r ".file.url")
+echo -e "$_spigotURL/$_apiDLurl"
+
+sleep $_sleepTime
+
+echo -e "\\npremium releases"
+
+#trademe
+_apiDLurl=$(curl -f -L -s --user-agent "$_userAgent" $_spigetURL/7544 | jq -r ".file.url")
+echo -e "$_spigotURL/$_apiDLurl"
+
+sleep $_sleepTime
+
+#residence
+_apiDLurl=$(curl -f -L -s --user-agent "$_userAgent" $_spigetURL/11480 | jq -r ".file.url")
+echo -e "$_spigotURL/$_apiDLurl"
+
+sleep $_sleepTime
+
+#tryme
+_apiDLurl=$(curl -f -L -s --user-agent "$_userAgent" $_spigetURL/3330 | jq -r ".file.url")
+echo -e "$_spigotURL/$_apiDLurl"
+
+sleep $_sleepTime
+
+#recount
+_apiDLurl=$(curl -f -L -s --user-agent "$_userAgent" $_spigetURL/3962 | jq -r ".file.url")
+echo -e "$_spigotURL/$_apiDLurl"
+
+sleep $_sleepTime
+
+#mobfarmmanager
+_apiDLurl=$(curl -f -L -s --user-agent "$_userAgent" $_spigetURL/15127 | jq -r ".file.url")
+echo -e "$_spigotURL/$_apiDLurl"
+
+sleep $_sleepTime
+
+#SelectionVisualizer
+_apiDLurl=$(curl -f -L -s --user-agent "$_userAgent" $_spigetURL/22631 | jq -r ".file.url")
+echo -e "$_spigotURL/$_apiDLurl"
+
+echo -e "\\nDone."
+```
